a652ebcb95c91339000c147a197c0e59f734a0ff
Benjamin Wiedemann <bwiedemann@gmail.student.tgm.ac.at>
2018-10-20 22:42:23 +0200

    decrypt

--- src/cipher/TranspositionCipher.java
+++ src/cipher/TranspositionCipher.java
@@ -1,32 +1,58 @@
 package cipher;
 
-public class TranspositionCipher implements Cipher{
+public class TranspositionCipher implements Cipher {
     private int transpositionLevel;
     private String eingabe;
-   public TranspositionCipher(int transpsitionLevel) throws Exception {
-       if (transpsitionLevel < 1) {
+
+    public TranspositionCipher(int transpsitionLevel) throws Exception {
+        if (transpsitionLevel < 1) {
             throw new Exception("Level must higher than 1");
-       }
-           this.transpositionLevel = transpsitionLevel;
-   }
+        }
+        this.transpositionLevel = transpsitionLevel;
+    }
 
     public int getTranspositionLevel() {
         return transpositionLevel;
     }
 
     public void setTranspositionLevel(int transpositionLevel) throws Exception {
-       if(transpositionLevel < 1){
-           throw new Exception("Level must higher than 1");
-       }
+        if (transpositionLevel < 1) {
+            throw new Exception("Level must higher than 1");
+        }
         this.transpositionLevel = transpositionLevel;
     }
 
 
     public String decrypt(String text) {
-       return ("g");
+        int zähler = text.length() - text.replace(" ", "").length();
+        if (zähler != this.transpositionLevel) {
+            return text;
+        }
+        String[] text2 = new String[this.transpositionLevel];
+        for (int i = 0; i < this.transpositionLevel; i++) {
+            int hilfe = text.indexOf(" ");
+            text2[i] = text.substring(0, text.lastIndexOf(" "));
+            i++;
+            if(i<this.transpositionLevel){
+                text = text.substring(text.indexOf(" ")+1,text.length());
+            }
+        }
+        int count = 0;
+        int[] counter = new int[this.transpositionLevel];
+        for (int i = 0; i<text.length()-this.transpositionLevel;i++){
+            int reset = count%this.transpositionLevel;
+            if(reset==0){
+                count=0;
+            }
+            text= text+text2[count].charAt(counter[count]);
+            counter[count]=counter[count]+1;
+            count++;
+        }
+        return text;
     }
+
     public String encrypt(String text) {
 
-        return("f") ;
+        return ("f");
     }
 }
